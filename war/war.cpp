08e9037e9c25e3cc9cfc14b3890aaf586ce3ed7d
Kevin O'Neil <kevinoneil43@gmail.com>
2019-01-24 21:24:27 -0500

    Initial commit for game of War

---
+++ war/war.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "war.hpp"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	//Making enum attributes of struct safeguards passing faulty values to the struct, therefore no need to catch
+	//faulty initialization with contructor.  
+	return 0;
+}
