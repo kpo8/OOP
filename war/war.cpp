8cf133aa1155e10df6246c9542c32509c3d606b8
Kevin O'Neil <kevinoneil43@gmail.com>
2019-02-05 17:59:02 -0500

    OOP:WAR: add duel function && add .cpp/.hpp files

--- war/war.cpp
+++ war/war.cpp
@@ -2,18 +2,14 @@
 #include<math.h>
 #include <algorithm>
 #include <vector>
-#include "war.hpp"
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::random_shuffle;
 
-// how many players are playing up to 4
-#define CARDPLAYER 2
-
-void generateDeck(vector<card>& deckOfCards);
-void shuffleDeck(vector<card>& deckOfCards);
+#include "warFunctions.hpp"
+#include "warFunctions.cpp"
 
 int main()
 {
@@ -22,39 +18,20 @@ int main()
 	//faulty initialization with contructor.
 	vector<card> deckOfCards;	
 	generateDeck(deckOfCards);
-	shuffleDeck(deckOfCards);
-	for(int i =0; i<52;++i)
-	{
-		cout << "suit: " << deckOfCards[i].cardSuit << " " << "rank: "<< deckOfCards[i].cardRank << endl;
-	}
+	shuffleDeck(deckOfCards);	
+	
+	//Our players
+	vector<card> player1;
+	vector<card> player2;
+	
+	assignCards(deckOfCards, player1, player2);
+	cout <<"player1" << endl;
+	spotCheck(player1);
+	cout <<"player2" <<endl;
+	spotCheck(player2);
+
+	duel(player1,player2);
 	
 	return 0;
 }
 
-/*This functuion geberates the carddeck
- * Takes vector of structs
- * shuffles deck
- * returns nothing
- */
-void shuffleDeck(vector<card>& deckOfCards)
-{
-	random_shuffle(deckOfCards.begin(), deckOfCards.end());
-}
-
-/*This function generates the carddeck
- * Takes array of pointers deckOfCards
- * This returns nothing
- */
-void generateDeck(vector <card>& deckOfCards)
-{
-	card pushBackCard;
-	for(int i=0; i < 4; ++i)
-	{
-		for(int j=0; j< 13; ++j)
-		{
-			pushBackCard.cardSuit = static_cast<suits>(i);
-			pushBackCard.cardRank = static_cast<rank>(j);
-			deckOfCards.push_back(pushBackCard); 
-		}
-	}
-}
---
+++ war/warFunctions.hpp
@@ -0,0 +1,59 @@
+#ifndef WARFUNCTIONS_H
+#define WARFUNCTIONS_H
+
+	//Type for suits
+	enum suits
+	{
+		hearts,
+		diamonds,
+		spades,
+		clubs
+	};
+	//type for ranks
+	enum rank
+	{
+		two,
+		three,
+		four,
+		five,
+		six,
+		seven,
+		eight,
+		nine,
+		ten,
+		jack,
+		queen,
+		king,
+		ace
+	};
+	
+	//struct for playing card
+	struct card
+	{
+		suits cardSuit;
+		rank  cardRank;
+		//leave in here in case something changes
+		/*card ()
+		{
+			//Guardrail to catch bad initialization of suit
+			if(cardSuit > 3 || cardSuit < 0) 
+			{
+				std::cout << "bad suit" << std::endl; 
+				abort();
+			}
+			//Guardrail to catch bad initilization of rank
+			if(cardSuit > 3 || cardSuit < 0) 
+			{
+				std::cout << "bad rank" << std::endl;
+				abort();
+			}
+		}*/
+	};
+void generateDeck(vector<card>& deckOfCards);
+void shuffleDeck(vector<card>& deckOfCards);
+void spotCheck(vector<card>& deckOfCards);
+void assignCards(vector<card>& deckOfCards, vector<card>& player1, vector<card>& player2);
+void duel(vector<card>& player1, vector<card>& player2);
+void warSpecialCase(vector<card>& player1, vector<card>& player2, card& player1Card, card& player2Card);
+
+#endif
