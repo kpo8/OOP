e721424320f9f1637ae4a9a1ec0a78d11bf46023
Kevin O'Neil <kevinoneil43@gmail.com>
2019-02-19 10:54:00 -0500

    lol

--- racing/main.cpp
+++ racing/main.cpp
@@ -1,49 +1,10 @@
 #include <cmath>
 
 #include <SFML/Graphics.hpp>
+
 using namespace sf;
 
-const int num = 8; // checkpoints
-int points[num][2] = { 300,  610,  1270, 430,  1380, 2380, 1900, 2460,
-                       1970, 1700, 2550, 1680, 2560, 3150, 500,  3300 };
-
-struct Car
-{
-	float x, y, speed, angle;
-	int n;
-
-	Car()
-	{
-		speed = 2;
-		angle = 0;
-		n = 0;
-	}
-
-	void move()
-	{
-		x += std::sin(angle) * speed;
-		y -= std::cos(angle) * speed;
-	}
-
-	void findTarget()
-	{
-		float tx = points[n][0];
-		float ty = points[n][1];
-		float beta = angle - std::atan2(tx - x, -ty + y);
-		if (std::sin(beta) < 0)
-		{
-			angle += 0.005 * speed;
-		}
-		else
-		{
-			angle -= 0.005 * speed;
-		}
-		if ((x - tx) * (x - tx) + (y - ty) * (y - ty) < 25 * 25)
-		{
-			n = (n + 1) % num;
-		}
-	}
-};
+#include "racing.hpp"
 
 int main()
 {
--- racing/racing.hpp
+++ racing/racing.hpp
@@ -2,6 +2,12 @@
 #ifndef RACING_HPP
 #define RACING_HPP
 
+const int num = 8; // checkpoints
+int points[num][2] = {
+                        300,  610,  1270, 430,  1380, 2380, 1900, 2460,
+                        1970, 1700, 2550, 1680, 2560, 3150, 500,  3300
+                     };
+
 //Class Car
 //variables x,y,speed,angle
 //methods move and findTarget
