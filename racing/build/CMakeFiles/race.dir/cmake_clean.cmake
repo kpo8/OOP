951b1db5b948aed7c0d1ed9a4568028cf8e9ecb8
Kevin O'Neil <kevinoneil43@gmail.com>
2019-03-31 21:18:50 -0400

    Project part 2 added

---
+++ racing/build/CMakeFiles/race.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/race.dir/main.cpp.o"
+  "race.pdb"
+  "race"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/race.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
